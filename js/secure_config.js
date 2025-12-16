f4ecdee2b535e5e2d032d0b1771889cf48bff2fa
Rajala Vishnu Sathwick <145008299+rvsr26@users.noreply.github.com>
2025-12-16 07:34:33 +0530

    Create secure_config.js

---
+++ js/secure_config.js
@@ -0,0 +1,5 @@
+// js/secure_config.js (THIS FILE STAYS LOCAL AND PRIVATE)
+export const privateConfig = {
+    apiKey: "AIzaSy...your-new-secure-key-here...",
+    // ... rest of the config here if needed
+};
